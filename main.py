802732463772a06875d92d08ecd4ca3a486b7e9c
Joshua Adkins <jadkins1999@gmail.com>
2023-10-11 23:26:14 -0500

    Math

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ torpedo_list = [
     "Type 2"
 ]
 
+torpedo_speed = {
+    "Type 1": 40,
+    "Type 2": 60
+}
 
 
 
@@ -15,6 +19,18 @@ root.title("Computer")
 root.attributes('-alpha', 0.75)
 root.attributes('-topmost', True)
 
+# == Functions ==
+
+def torpedo_velocity (torpedo_name):
+    return torpedo_speed[torpedo_name]
+    
+
+def gyro_calculate(gyro_box, impact_angle, torpedo_v, bearing_angle, bow_angle, target_v):
+    defl = math.asin( (float(target_v) / float(torpedo_v)) * math.sin( math.radians(float(bow_angle)) ) )
+    defl = round(math.degrees(defl),2)
+    print(f"Deflection Angle: {defl}")
+    gyro_box.insert(0, str(float(bearing_angle) - defl))
+
 
 # == Element Setups ==
 left_dial = Meter(root, fg="#242424", radius=250, start=180, end=-180,
@@ -22,11 +38,6 @@ left_dial = Meter(root, fg="#242424", radius=250, start=180, end=-180,
                start_angle=270, end_angle=360, scale_color="white", axis_color="cyan",
                needle_color="white",  scroll_steps=3)
 left_dial.set(0)
-
-
-gyro_lab = Label(master=root, text="Gyro")
-gyro_box = Entry(root)
-gyro_button = Button(master=root, text="Calculate")
 
 
 torpedo_type_lab = Label(master=root, text="Torpedo")
@@ -43,16 +54,22 @@ bearing_angle_box = Entry(root)
 bow_angle_lab = Label(master=root, text="Angle Off Bow")
 bow_angle_box = Entry(root)
 
-bl_dial = Meter(root, fg="#242424", radius=250, start=180, end=-180,
+
+impact_dial = Meter(root, fg="#242424", radius=250, start=180, end=-180,
                major_divisions=30, minor_divisions=3, border_width=0, text_color="white",
                start_angle=270, end_angle=360, scale_color="white", axis_color="cyan",
                needle_color="white",  scroll_steps=3)
-bl_dial.set(0)
+impact_dial.set(0)
 
 time_lab = Label(master=root, text="Time On Target")
 time_box = Entry(root)
 
 periscope = Button(master=root, text="Periscope Slaving")
+
+
+gyro_lab = Label(master=root, text="Gyro")
+gyro_box = Entry(root)
+gyro_button = Button(master=root, text="Calculate", command=lambda: gyro_calculate(gyro_box, impact_dial.value, torpedo_velocity(torpedo_var.get()), bearing_angle_box.get(), bow_angle_box.get(), target_v_box.get()))
 
 # == Layouts ==
 left_dial.grid(row=0, column=1, rowspan=4, pady=5)
@@ -70,7 +87,7 @@ bearing_angle_box.grid(row = 2, column = 5, sticky=N)
 bow_angle_lab.grid(row = 3, column = 4, sticky=N)
 bow_angle_box.grid(row = 3, column = 5, sticky=N)
 
-bl_dial.grid(row=4, column=1, pady=7)
+impact_dial.grid(row=4, column=1, pady=7)
 time_lab.grid(row=4, column=2)
 time_box.grid(row=4, column=3)
 periscope.grid(row=4, column=4, columnspan=2)
